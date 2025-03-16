acdc6d28f68e324600c849b26dd904ef5f254c8d
Campoos <joaosantos20071009@gmail.com>
2025-03-15 22:18:22 -0300

    html/css da tela home feito

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 "use strict"
+
+const linkMonstros = document.getElementById('redirecionamentoMonstros')
+
+function telaMonstros(){
+    const containerHome = document.getElementById('containerHome')
+
+    containerHome.innerHTML = ""
+}
+
+linkMonstros.addEventListener('click', telaMonstros)
